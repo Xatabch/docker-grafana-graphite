f99c297389300e96a4d0fe82e48d8e7685995293
Ivan <ivan.morozov@list.ru>
2020-06-07 03:45:46 +0300

    master: stay only gauge

--- server/index.js
+++ server/index.js
@@ -17,30 +17,6 @@ app.post('/gauge', (req, res) => {
     res.status(200).send();
 });
 
-app.post('/histogram', (req, res) => {
-    const name = req.body.name;
-    const value = req.body.value;
-    const tag = req.body.tag;
-
-    sdc.histogram(name, value, tag);
-    res.status(200).send();
-});
-
-app.post('/set', (req, res) => {
-    const name = req.body.name;
-    const value = req.body.value;
-
-    sdc.set(name, value);
-    res.status(200).send();
-});
-
-app.post('/raw', (req, res) => {
-    const value = req.body.value;
-
-    sdc.raw(value);
-    res.status(200).send();
-});
-
 app.listen(port, () => {
     console.log(`Application is listening on port: ${port}`);
 })
